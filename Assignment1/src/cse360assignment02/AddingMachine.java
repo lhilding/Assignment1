8c03e39a4cca55067b1147cb0e8d2641e5f28ee1
Leif Hilding <lhilding@asu.edu>
2020-10-07 20:39:53 -0700

    Initial checkin of AddingMachine

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,35 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	  private int total;
+	  private String history;
+	  
+	  /**This is a constructor used to create an instance of AddingMachine*/
+	  public AddingMachine () {
+	    total = 0; 
+	    history = "";// not needed - included for clarity
+	  }
+	  
+	  /**This method returns the value of the private variable total*/
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**This method adds a value to the total*/
+	  public void add (int value) {
+	  }
+
+	  /**This method subtracts a value to the total*/
+	  public void subtract (int value) {
+	  }
+
+	  /**This toString method will be called when an instance of 
+	     adding machine is printed, returns history*/
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**This clears the total and the history*/
+	  public void clear() {
+	  }
+}
